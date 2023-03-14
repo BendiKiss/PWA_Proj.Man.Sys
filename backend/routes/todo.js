f4247fe055964bf3568f63157abff9c9c79713b5
DaddyBendi <bendguzkiss1818@gmail.com>
2023-03-14 12:54:18 +0100

    Front- and Backend set up and working

---
+++ backend/routes/todo.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const Todo = require('../models/Todos')
+
+// Get All Todo route
+router.get('/', async (req, res) => {
+    const todos = await Todo.find();
+    res.json(todos)
+})

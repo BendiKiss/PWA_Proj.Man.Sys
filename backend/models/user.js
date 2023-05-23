8f4003f145b6e4c0fb1c0efe46fd742c89078903
DaddyBendi <bendguzkiss1818@gmail.com>
2023-05-23 09:49:58 +0200

    final push

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-
 let userSchema = new Schema(
     {
         name:       { type: String, required: true, min: 5,max: 150 },
